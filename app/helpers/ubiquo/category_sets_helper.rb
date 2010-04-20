96171a6cfdbd155bd7b76528df71c56383309e52
Bernat Foj Capell <bfoj@gnuine.com>
2010-04-20 10:01:35 +0200

    Do not show edit options if :administrable_category_sets is false

--- app/helpers/ubiquo/category_sets_helper.rb
+++ app/helpers/ubiquo/category_sets_helper.rb
@@ -20,20 +20,20 @@ module Ubiquo::CategorySetsHelper
   def category_set_list(collection, pages, options = {})
     render(:partial => "shared/ubiquo/lists/standard", :locals => {
         :name => 'category_set',
-        :headers => [:name, :key],
+        :headers => options[:can_manage] ? [:name, :key] : [:name],
         :rows => collection.collect do |category_set| 
           {
             :id => category_set.id, 
-            :columns => [
-              category_set.name,
-              category_set.key,
-            ],
+            :columns => category_set_column_values(category_set, options),
             :actions => category_set_actions(category_set, options)
           }
         end,
         :pages => pages,
-        :link_to_new => link_to(t("ubiquo.category_set.index.new"),
-                                new_ubiquo_category_set_path, :class => 'new')
+        :link_to_new => (
+          link_to(
+            t("ubiquo.category_set.index.new"),
+            new_ubiquo_category_set_path, :class => 'new'
+          ) if options[:can_manage])
       })
   end
     
@@ -47,5 +47,11 @@ module Ubiquo::CategorySetsHelper
       :confirm => t("ubiquo.category_set.index.confirm_removal"), :method => :delete
       ) if options[:can_manage]
     actions
+  end
+
+  def category_set_column_values(category_set, options = {})
+    values = [category_set.name]
+    values << category_set.key if options[:can_manage]
+    values
   end
 end
